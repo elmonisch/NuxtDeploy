9570fbc921005ed08a1036980161996badeb4128
elmonisch <hasifzulkifli95@gmail.com>
2020-05-09 23:16:03 +0800

    up

--- pages/project.vue
+++ pages/project.vue
@@ -1,5 +1,25 @@
 <template>
-    <div class="containers">
-        <h3>Tak siap lagi</h3>
-    </div>
+  <div class="containers">
+    <nav>
+          <input type="checkbox" id="nav" class="hidden">
+          <label for="nav" class="nav-btn">
+                <i></i>
+                <i></i>
+                <i></i>
+          </label>
+          <div class="logo">
+                <nuxt-link to="/">HelloHasif</nuxt-link>
+          </div>
+          <div class="nav-wrapper">
+                <ul>
+                      <li><nuxt-link to="/about">About</nuxt-link></li>
+                      <li><nuxt-link to="/project">Projects</nuxt-link></li>
+                      <li><nuxt-link to="/blog">Blogs</nuxt-link></li>
+                      <li><nuxt-link to="/contact">Contact</nuxt-link></li>
+                </ul>
+          </div>
+    </nav>
+
+    
+  </div>
 </template>
